d6f8a9928bdf18fd530313d3d72ce8f369ce6f7d
Logan Martinez <logan_amp@outlook.com>
2024-04-01 15:17:25 -0600

    Working HUD, clamped position, new game_over
    Restart and exit buttons working, clamped the position of player, added game over on lower screen bound.

--- HUD.gd
+++ HUD.gd
@@ -3,12 +3,12 @@ extends CanvasLayer
 @onready var score_label = $"Score Label"
 @onready var center_container = $CenterContainer
 @onready var end_score_label = $"CenterContainer/VBoxContainer/End Score Label"
-
-
-var score = 0
+@onready var restart_button = $"CenterContainer/VBoxContainer/Restart Button"
+@onready var score = 0
 
 
 func _ready():
+	get_tree().paused = false
 	score_label.text = str(score)
 	center_container.hide()
 
@@ -22,3 +22,17 @@ func _on_tubos_game_over():
 	end_score_label.text = "Score:" + str(score)
 	center_container.show()
 	score_label.hide()
+
+
+func _on_player_game_over():
+	end_score_label.text = "Score:" + str(score)
+	center_container.show()
+	score_label.hide()
+
+
+func _on_restart_button_button_up():
+	get_tree().reload_current_scene()
+
+
+func _on_exit_button_button_up():
+	get_tree().quit(0)
